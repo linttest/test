47fa526a9f9d62202f26ebfed8c8894546434cfa
Michal Lomnicki <michal.lomnicki@gmail.com>
2014-02-11 00:47:53 +0100

    so bad

---
+++ sobad.rb
@@ -0,0 +1,32 @@
+
+class Oh_My_God
+
+  def initialize unused
+  end
+
+  def FooBar
+
+  end
+
+  def love_trailing_ws     
+    a = 1
+        
+  end
+
+  def why_indent
+          niah_niah = a = 2
+      end
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
